d1c5a67f3bcc28620904d75e8d29283f972a8a9f
Sakil <mdsakiluzzaman@gmail.com>
2022-02-01 12:26:50 +0600

    first push

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = (props) => {
+    return (
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 ">
+            <img alt="robots" src={`https://robohash.org/${props.id}?200x200`}></img>
+            <div>
+                <h2>{props.name}</h2>
+                <h2>{props.email}</h2>
+
+
+            </div>
+        </div>
+    );
+
+}
+
+export default Card;
